7f5feeb30f00dd0498030f2c6e862da2d79a6a3f
Shaun <srswarup_b21@ce.vjti.ac.in>
2023-01-26 18:33:01 +0530

    Update MongoDB Atlas

---
+++ server/mongoupdate.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import json
+
+client = MongoClient(
+    'mongodb+srv://Comparazon:sharayu2000@cluster0.vkvevwe.mongodb.net/?retryWrites=true&w=majority')
+db = client.get_database('shaurya-test')
+records = db.products
+
+with open('/home/shaun/Desktop/Ecommerce-Scraper/final-flipkart-amazon-data.json', 'r') as json_file:
+    data = json.loads(json_file.read())
+
+if isinstance(data, list):
+    records.delete_many({})
+    records.insert_many(data)
+else:
+    records.delete_many({})
+    records.insert_one(data)
